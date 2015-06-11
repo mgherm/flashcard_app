a885364a637286970ff9ad6910322da35d4c54b4
mgherm <herman@cictr.com>
2015-06-11 07:11:31 -0400

    new flashcard app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlashcardApp::Application.config.secret_token = '61debd12ffb6395f4521032a2989f645c2a7e9191ef028e968561cf2585168c83ebdf0c530879fe8ebdb9c0676c771b51d62d184224d6f64abc20a2e7db0c0d6'
---
+++ test/unit/helpers/decks_controller_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class DecksControllerHelperTest < ActionView::TestCase
+end
